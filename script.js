6cca6dab25302d15177cf744f0bab6e7f6cb2f07
aveencoder <aveen598@gmail.com>
2025-08-15 21:44:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 //complete this code
-class Animal {}
+class Animal {
+	  private _species:string;
+    
+    constructor (species:string){
+        this._species = species;
+    }
 
-class Dog extends Animal {}
+    get species():string{
+        return`The ${this._species} of the animal`;
+    }
+    
+    makeSound():void{
+          console.log(`The ${this._species} makes a sound`);
+    }
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+	 purr():void{
+      console.log("purr");
+    }
+}
+
+class Cat extends Animal {
+	 bark(): void{
+      console.log("woof");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
